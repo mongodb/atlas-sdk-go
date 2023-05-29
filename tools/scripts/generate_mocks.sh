9a4252a436e963b4fa5a752cc32b9379f926d025
wtrocki <w.trocki@mongodb.com>
2023-05-29 14:47:51 +0200

    fix: mocks

---
+++ tools/scripts/generate_mocks.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+
+MOCK_FOLDER=${MOCK_FOLDER:-../admin}
+MOQ_INSTALLED=$(command -v moq)
+
+if [[ -z $MOQ_INSTALLED ]]; then
+  echo "moq is not installed. Installing moq to continue."
+  go install github.com/matryer/moq@latest
+fi
+
+
+FILES=$(find $MOCK_FOLDER -type f -name "api*.go")
+
+for FILE in $FILES; do
+  FILENAME=$(basename $FILE)
+  FILENAME_API="${FILENAME%.*}API"
+  FILENAME_MOCK="${FILENAME%.*}_mock.go"
+  
+  moq -out "$MOCK_FOLDER"/"$FILENAME_MOCK" "$MOCK_FOLDER" "$FILENAME_API"
+done
