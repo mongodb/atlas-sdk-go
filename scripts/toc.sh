f2674ead4e17d1082f30f57d903b862f856f6f5f
wtrocki <w.trocki@mongodb.com>
2023-05-29 22:47:39 +0200

    fix: make script paths configurable

--- scripts/toc.sh
+++ scripts/toc.sh
@@ -2,16 +2,29 @@
 set -o errexit
 set -o nounset
 
-if which "./scripts/gh-md-toc" >/dev/null 2>&1; then
+BIN_FOLDER=./bin
+DOCS_FOLDER=./docs
+
+pushd $BIN_FOLDER
+
+if which "./gh-md-toc" >/dev/null 2>&1; then
   echo "Binary gh-md-toc exists."
 else
   echo Downloading scripts
-  pushd ./scripts 
+
   wget https://raw.githubusercontent.com/ekalinin/github-markdown-toc/master/gh-md-toc
   chmod a+x gh-md-toc
-  popd
 fi
+
+docsbin=$(readlink -f "./gh-md-toc")
+
+popd
 
 echo "generating table of contents"
 echo "# Atlas SDK GO documentation" > ./docs/README.md
-cd  ./docs/ && ../scripts/gh-md-toc --skip-header ./doc_*.md  >> ./README.md
+
+pushd $DOCS_FOLDER
+
+$docsbin --skip-header ./doc_*.md  >> ./README.md
+
+popd
