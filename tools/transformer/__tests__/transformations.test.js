855afcb276a3e4c1d69f4c74624d091f0f21d459
Ma77C0nd0n <matt.condon@mongodb.com>
2023-06-13 10:40:44 +0100

    fix unit test

--- tools/transformer/__tests__/transformations.test.js
+++ tools/transformer/__tests__/transformations.test.js
@@ -74,6 +74,6 @@ test("applyAddExperimentalToDescriptions for stable operationId", () => {
 test("applyAddExperimentalToDescriptions for experimental operationId", () => {
   api = applyAddExperimentalToDescriptions(api);
   expect(api.paths["/api/atlas/v2/example/info"].get.description).toEqual(
-    "(experimental) Info description."
+    "[experimental] Info description."
   );
 });
