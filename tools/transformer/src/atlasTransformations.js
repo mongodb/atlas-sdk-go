50872152271d2a1822224a65cccc746dd984d902
wtrocki <w.trocki@mongodb.com>
2023-06-21 21:23:37 +0200

    fix: format

--- tools/transformer/src/atlasTransformations.js
+++ tools/transformer/src/atlasTransformations.js
@@ -28,7 +28,7 @@ module.exports = function runTransformations(openapi) {
     ".components.schemas.ApiAtlasEndpointServiceView",
 
     ".components.schemas.ServerlessMetricThreshold",
-  
+
     // Renamed objects
     ".components.schemas.AlertNotificationViewGroup",
     ".components.schemas.ServerlessTenantEndpoint",
