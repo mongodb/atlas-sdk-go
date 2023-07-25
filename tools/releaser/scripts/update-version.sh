678fbaf5cbeef096cf8fa3b8d40ff85d69d83da7
wtrocki <w.trocki@mongodb.com>
2023-07-25 19:14:39 +0200

    fix: remove echo

--- tools/releaser/scripts/update-version.sh
+++ tools/releaser/scripts/update-version.sh
@@ -11,7 +11,6 @@ echo "Current Resource Version: $NEW_RESOURCE_VERSION"
 major_version_breaking_changes="${SDK_MAJOR_VERSION: -3}"
 major_version_bump=$((major_version_breaking_changes+1))
 major_version_bump=$(printf "%03d" "$major_version_bump")
-echo $major_version_bump
 BUMPED_MAJOR_VERSION="v${SDK_RESOURCE_VERSION}${major_version_bump}"
 
 echo "Modifying $SDK_MAJOR_VERSION to $BUMPED_MAJOR_VERSION Resource Version across the repository."
