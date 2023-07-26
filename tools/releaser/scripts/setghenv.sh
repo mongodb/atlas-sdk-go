bb326944babb124019026d890fd1be95d92a7b6b
wtrocki <w.trocki@mongodb.com>
2023-07-26 21:31:02 +0200

    fix: support multiline string

--- tools/releaser/scripts/setghenv.sh
+++ tools/releaser/scripts/setghenv.sh
@@ -17,8 +17,12 @@ echo "SDK_VERSION=${SDK_VERSION}" >> $GITHUB_ENV
 echo "HYPEN_RESOURCE_VERSION=${HYPEN_RESOURCE_VERSION}" >> $GITHUB_ENV
 echo "RELEASE_TAG=${SDK_VERSION}" >> $GITHUB_ENV
 
+EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
+
 RELEASE_NOTES=$(envsubst < "$script_path/../templates/RELEASE_NOTES.tmpl")
 RELEASE_NOTES=$RELEASE_NOTES\n$(cat $script_path/../breaking_changes/${SDK_MAJOR_VERSION}.md)
 
-echo 'RELEASE_NOTES="${RELEASE_NOTES}' >> $GITHUB_ENV
-echo $RELEASE_NOTES
+## Multiline string
+echo "RELEASE_NOTES<<$EOF" >> $GITHUB_ENV
+echo $RELEASE_NOTES >> $GITHUB_ENV
+echo "$EOF" >> "$GITHUB_ENV"
