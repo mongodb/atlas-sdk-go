f450676d447ed4230038eeae6d9be761fd68d458
wtrocki <w.trocki@mongodb.com>
2023-08-02 16:05:46 +0200

    fix: s3 update

--- tools/releaser/scripts/s3.sh
+++ tools/releaser/scripts/s3.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -ueo pipefail
+
 ## Internal script
 # Not to be used directly
 
@@ -5,13 +8,15 @@ script_path=$(dirname "$0")
 # shellcheck source=/dev/null
 source "$script_path/extract-version.sh"
 
-S3_BUCKET="s3://mongodb-golang-url/atlas-sdk/"
-LOCAL_FILE_PATH="/path/to/local/index.html"
+S3_BUCKET="mongodb-golang-url"
+LOCAL_FILE_PATH="$script_path/../resources/index.html"
 ## Indentionally left as manual file to avoid accidental upload/values
-NEW_FOLDER_NAME="v20230201005"
+NEW_FOLDER_NAME="atlas-sdk/v20230201005"
+
+aws s3api head-object --bucket "$S3_BUCKET" --key "atlas-sdk/index.html"
 
 # Create the new folder with the specified name
-aws s3api put-object --bucket "$S3_BUCKET" --key "$NEW_FOLDER_NAME/" --acl private
+aws s3api put-object --bucket "$S3_BUCKET" --key "$NEW_FOLDER_NAME/"
 
 # Upload the local file to the newly created folder
 aws s3 cp "$LOCAL_FILE_PATH" "$S3_BUCKET$NEW_FOLDER_NAME/"
