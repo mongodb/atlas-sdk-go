0d54bc8bf64517e6150c7a2fcf276339e8f759c2
wtrocki <w.trocki@mongodb.com>
2023-08-16 18:53:28 +0200

    CLOUDP-195429: generate breaking changes tests

--- tools/scripts/generate_tests.sh
+++ tools/scripts/generate_tests.sh
@@ -7,7 +7,7 @@ OPENAPI_FILE_NAME=${OPENAPI_FILE_NAME:-atlas-api.yaml}
 FOLDER=${FOLDER:-../test/generated}
 
 transformed_file="atlas-api-transformed.yaml"
-client_package="admin"
+client_package="test"
 openapiFileLocation="$OPENAPI_FOLDER/$transformed_file"
 
 echo "# Running Test Generation"
@@ -18,3 +18,16 @@ npm exec openapi-generator-cli -- generate \
     --type-mappings=integer=int \
     --ignore-file-override=config/.go-ignore-tests
 
+pushd $FOLDER
+for file in *; do
+    if [[ ! -f "$file" ]]; then
+        continue
+    fi
+
+    if [[ ! "$file" == *.* ]]; then
+        new_name="${file}.go"
+        mv "$file" "$client_package/$new_name"
+        echo "Added extension to: $new_name"
+    fi
+done
+popd
