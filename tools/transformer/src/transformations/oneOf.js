984f330f2fd7f6624fa9f86680f9b939cce23ff4
Wojciech Trocki <w.trocki@mongodb.com>
2024-01-17 11:35:40 +0100

    task: fix message in transformation engine (#259)

--- tools/transformer/src/transformations/oneOf.js
+++ tools/transformer/src/transformations/oneOf.js
@@ -104,6 +104,12 @@ function transformOneOfProperties(parentObject, api) {
         }
       } else {
         // Otherwise this situation require human intervention
+        console.error(
+          "OpenAPI object is missing properties or allOf field. This is usually an error in the OpenAPI spec."
+        );
+        console.error(
+          "Please ensure that elements of oneOf schema are objects (classes) instead of single types (bool, string etc.)."
+        );
         throw new Error(`${JSON.stringify(childObject, "", 2)}`);
       }
     }
