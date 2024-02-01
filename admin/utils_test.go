d7feb4d7edd8ac749f73785dfead8ac165e71134
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2024-02-01 15:40:01 +0100

    admin/utils.go: add helper methods for parsing and pointer handling

---
+++ admin/utils_test.go
@@ -0,0 +1,101 @@
+package admin
+
+import (
+	"testing"
+	"time"
+)
+
+type testCase[T comparable] struct {
+	name         string
+	val          T
+	defaultValue T
+	wantNil      bool
+}
+
+func assertSetOrDefault[T comparable](t *testing.T, tc testCase[T]) {
+	t.Run(tc.name, func(t *testing.T) {
+		ptr := SetOrDefault(tc.val, tc.defaultValue)
+		if gotNil := ptr == nil; gotNil != tc.wantNil {
+			t.Errorf("got nil %t, want %t", gotNil, tc.wantNil)
+		}
+	})
+}
+
+func TestSetOrDefault(t *testing.T) {
+	assertSetOrDefault(t, testCase[int]{
+		name:         "non default int",
+		val:          1,
+		defaultValue: 0,
+		wantNil:      false,
+	})
+
+	assertSetOrDefault(t, testCase[int]{
+		name:         "default int",
+		val:          0,
+		defaultValue: 0,
+		wantNil:      true,
+	})
+
+	assertSetOrDefault(t, testCase[string]{
+		name:         "non default string",
+		val:          "hello",
+		defaultValue: "",
+		wantNil:      false,
+	})
+
+	assertSetOrDefault(t, testCase[string]{
+		name:         "default string",
+		val:          "",
+		defaultValue: "",
+		wantNil:      true,
+	})
+}
+
+func TestStringToTime(t *testing.T) {
+	for _, tc := range []struct {
+		name  string
+		input string
+
+		want    time.Time
+		wantErr string
+	}{
+		{
+			name:  "valid time",
+			input: "2023-07-18T16:12:23Z",
+			want: time.Date(
+				2023, 7, 18,
+				16, 12, 23, 0,
+				time.UTC,
+			),
+		},
+		{
+			name:  "valid time with millis",
+			input: "2023-07-18T16:12:23.456Z",
+			want: time.Date(
+				2023, 7, 18,
+				16, 12, 23, 456_000_000,
+				time.UTC,
+			),
+		},
+		{
+			name:    "invalid time",
+			input:   "invalid",
+			wantErr: `parsing time "invalid" as "2006-01-02T15:04:05.999999999Z07:00": cannot parse "invalid" as "2006"`,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			gotErr := ""
+			got, err := StringToTime(tc.input)
+			if err != nil {
+				gotErr = err.Error()
+			}
+
+			if gotErr != tc.wantErr {
+				t.Errorf("want error %q, got %q", tc.wantErr, gotErr)
+			}
+			if got != tc.want {
+				t.Errorf("want %v, got %v", tc.want, got)
+			}
+		})
+	}
+}
