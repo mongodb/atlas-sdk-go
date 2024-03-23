ad27ab20490ed40d2c4c14863aa34583c2a5cccf
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-03-23 18:04:21 +0100

    mockery config

--- tools/releaser/scripts/version-paths.sh
+++ tools/releaser/scripts/version-paths.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export VERSION_UPDATE_PATHS="../**/*.go,../**/*.md,../tools/**/*.mustache,../go.mod,../.github/workflows/autoupdate-dev.yaml"
+export VERSION_UPDATE_PATHS="../**/*.go,../**/*.md,../tools/**/*.mustache,../go.mod,../.mockery.yaml"
