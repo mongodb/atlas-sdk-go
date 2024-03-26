fbf8e6465cbe44e86f8535da2988ea36220ea148
wtrocki <w.trocki@mongodb.com>
2024-03-26 16:50:52 +0100

    CLOUDP-237245: retry example

---
+++ examples/retry/retry.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"context"
+
+	"go.mongodb.org/atlas-sdk/v20231115008/admin"
+	"go.mongodb.org/atlas-sdk/v20231115008/examples"
+
+	retryablehttp "github.com/hashicorp/go-retryablehttp"
+	"github.com/mongodb-forks/digest"
+)
+
+/*
+* MongoDB Atlas Go SDK Retryable Request Example
+*
+* Example using custom http client that handles rate limiting and 500 Http errors by retrying requests automatically.
+* Example uses https://pkg.go.dev/github.com/hashicorp/go-retryablehttp.
+* Please refer to the package documentation for more information.
+ */
+func main() {
+	ctx := context.Background()
+	// Values provided as part of env variables
+	// See: https://www.mongodb.com/docs/atlas/app-services/authentication/api-key/
+	apiKey := os.Getenv("MONGODB_ATLAS_PUBLIC_KEY")
+	apiSecret := os.Getenv("MONGODB_ATLAS_PRIVATE_KEY")
+	url := os.Getenv("MONGODB_ATLAS_URL")
+
+	// Using custom client 
+	// This example relies on https://pkg.go.dev/github.com/hashicorp/go-retryablehttp
+	// retryablehttp performs automatic retries under certain conditions. 
+	// Mainly, if an error is returned by the client (connection errors etc), 
+	/// or if a 500-range response is received, then a retry is invoked.
+	retryClient := retryablehttp.NewClient()
+	retryClient.RetryMax = 3
+
+	var transport http.RoundTripper = &retryablehttp.RoundTripper{Client: retryClient};
+	digestRetryAbleTransport := &digest.Transport{
+		Username:  apiKey,
+		Password:  apiSecret,
+		Transport: transport,
+	}
+	retryableClient, err := digestRetryAbleTransport.Client()
+	if err != nil{
+		log.Fatal("Cannot instantiate client")
+	}
+	sdk, err := admin.NewClient(
+		admin.UseHTTPClient(retryableClient),
+		admin.UseBaseURL(url),
+		admin.UseDebug(false))
+	examples.HandleErr(err, nil)
+
+	request := sdk.ProjectsApi.ListProjectsWithParams(ctx,
+		&admin.ListProjectsApiParams{
+			ItemsPerPage: admin.PtrInt(1),
+			IncludeCount: admin.PtrBool(true),
+			PageNum:      admin.PtrInt(1),
+		})
+	projects, response, err := request.Execute()
+	examples.HandleErr(err, response)
+
+	if projects.GetTotalCount() == 0 {
+		log.Fatal("account should have at least single project")
+	}
+
+}
--- go.mod
+++ go.mod
@@ -8,6 +8,11 @@ require (
 )
 
 require (
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.5 // indirect
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
