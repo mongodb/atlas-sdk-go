c1df4ddf7a42c7f2450c0bcb78e8be15b5202515
wtrocki <w.trocki@mongodb.com>
2024-03-27 19:26:24 +0100

    fix: retry options

--- examples/retry/retry.go
+++ examples/retry/retry.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -28,23 +29,23 @@ func main() {
 	apiSecret := os.Getenv("MONGODB_ATLAS_PRIVATE_KEY")
 	url := os.Getenv("MONGODB_ATLAS_URL")
 
-	// Using custom client 
+	// Using custom client
 	// This example relies on https://pkg.go.dev/github.com/hashicorp/go-retryablehttp
-	// retryablehttp performs automatic retries under certain conditions. 
-	// Mainly, if an error is returned by the client (connection errors etc), 
+	// retryablehttp performs automatic retries under certain conditions.
+	// Mainly, if an error is returned by the client (connection errors etc),
 	/// or if a 500-range response is received, then a retry is invoked.
 	retryClient := retryablehttp.NewClient()
 	retryClient.RetryMax = 3
 
 	retryableClient, err := newRetryableClient(retryClient, apiKey, apiSecret)
-	if err != nil{
+	if err != nil {
 		log.Fatal("Cannot instantiate client")
 	}
 	sdk, err := admin.NewClient(
 		admin.UseHTTPClient(retryableClient),
 		admin.UseBaseURL(url),
 		admin.UseDebug(false))
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 
@@ -55,16 +56,16 @@ func main() {
 			PageNum:      admin.PtrInt(1),
 		})
 	projects, _, err := request.Execute()
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Info("Total Projects", projects.GetTotalCount())
+	fmt.Println("Total Projects", projects.GetTotalCount())
 
 }
 
 func newRetryableClient(retryClient *retryablehttp.Client, apiKey string, apiSecret string) (*http.Client, error) {
 	var transport http.RoundTripper = &retryablehttp.RoundTripper{Client: retryClient}
-	digestRetryAbleTransport := digest.NewTransportWithHTTPRoundTripper(apiKey, apiSecret,transport)
+	digestRetryAbleTransport := digest.NewTransportWithHTTPRoundTripper(apiKey, apiSecret, transport)
 	return digestRetryAbleTransport.Client()
 }
