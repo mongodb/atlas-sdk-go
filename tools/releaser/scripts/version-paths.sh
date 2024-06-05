3a8c77840353acb148e9c20bb66a0b828ea3f6f9
Wojciech Trocki <w.trocki@mongodb.com>
2024-06-05 13:40:43 +0200

    task: update examples go mod file (#337)

--- tools/releaser/scripts/version-paths.sh
+++ tools/releaser/scripts/version-paths.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export VERSION_UPDATE_PATHS="../**/*.go,../**/*.md,../tools/**/*.mustache,../go.mod,../.github/workflows/autoupdate-preview.yaml,../.mockery.yaml"
+export VERSION_UPDATE_PATHS="../**/*.go,../**/*.md,../tools/**/*.mustache,../**/go.mod,../.github/workflows/autoupdate-preview.yaml,../.mockery.yaml"
