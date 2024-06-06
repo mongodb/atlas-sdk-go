05b8aa4ee0ebf514aa55bb7dd008e0d373266759
wtrocki <w.trocki@mongodb.com>
2024-06-06 17:34:42 +0200

    fix: initial transformation

--- tools/transformer/src/atlasTransformations.js
+++ tools/transformer/src/atlasTransformations.js
@@ -18,6 +18,8 @@ const ignoredModelNames = require("./name.ignore.json").ignoreModels;
  * Function specifies list of transformations to run
  */
 module.exports = function runTransformations(openapi) {
+  openapi = searchAPIIssuesTransformation(openapi);
+
   openapi = applyDiscriminatorTransformations(openapi);
   openapi = applyOneOfTransformations(openapi);
   openapi = applyAnyOfTransformations(openapi);
@@ -67,3 +69,46 @@ module.exports = function runTransformations(openapi) {
 
   return openapi;
 };
+
+// Workarounds for issues in the search API
+function searchAPIIssuesTransformation(openapi) {
+  // API is overly complex in this case and provides no value as typed interface
+  if (openapi.components.schemas.SearchIndexResponse) {
+    responseParent = openapi.components.schemas.SearchIndexResponse;
+    if (responseParent.properties.latestDefinition) {
+      if(responseParent.discriminator.mapping){
+        responseParent.properties.latestDefinition = {oneOf: []};
+        for(mappingKey in responseParent.discriminator.mapping){
+          var ref = responseParent.discriminator.mapping[mappingKey];
+          var reference = resolveOpenAPIReference(openapi,ref)
+          responseParent.properties.latestDefinition.oneOf.push({
+            $ref: reference.allOf[1].properties.latestDefinition.$ref
+          });
+          delete reference.allOf[1].properties.latestDefinition
+        }
+      }
+    }
+  }
+  console.error("SearchIndexResponse", openapi.components.schemas.SearchIndexResponse)
+  console.error("VectorSearchIndexResponse", openapi.components.schemas.VectorSearchIndexResponse)
+  console.error("TextSearchIndexResponse", openapi.components.schemas.TextSearchIndexResponse)
+  return openapi;
+}
+
+function resolveOpenAPIReference(openapi, ref) {
+  if (!ref.startsWith("#/")) {
+    throw new Error("Invalid reference format: " + ref);
+  }
+  const parts = ref.split("/");
+  // Skip the first empty part
+  parts.shift();
+
+  let current = openapi;
+  for (const part of parts) {
+    if (!current.hasOwnProperty(part)) {
+      throw new Error("Reference not found: " + ref);
+    }
+    current = current[part];
+  }
+  return current;
+}
