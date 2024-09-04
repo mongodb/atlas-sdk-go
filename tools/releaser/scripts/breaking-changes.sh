ce72faf3f0172803bee3d14a001ca873e1ba65d5
wtrocki <w.trocki@mongodb.com>
2024-09-04 10:17:46 +0200

    fix: simplify breaking changes job

--- tools/releaser/scripts/breaking-changes.sh
+++ tools/releaser/scripts/breaking-changes.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-set +e
-set +u
+set -eu
 GOPATH=$(go env GOPATH)
 
 # Inputs:
@@ -12,23 +11,36 @@ script_path=$(dirname "$0")
 echo "Installing go-apidiff"
 go install github.com/joelanford/go-apidiff@latest > /dev/null
 
-GIT_BASE_REF=${GIT_BASE_REF:-git rev-parse head || echo}
+current_ref=$(git rev-parse main || echo)
+GIT_BASE_REF=${GIT_BASE_REF:-$current_ref}
+TARGET_BREAKING_CHANGES_FILE=${TARGET_BREAKING_CHANGES_FILE:""}
 
 echo "Running breaking changes check for $GIT_BASE_REF"
 
 pushd "$script_path/../../../" || exit ## workaround for --repo-path="../" not working
-BREAKING_CHANGES=$("$GOPATH/bin/go-apidiff" "$GIT_BASE_REF" --compare-imports="false" --print-compatible="false")
-popd || exit
+echo "Changed directory to $(pwd)"
+set +e
+BREAKING_CHANGES=$("$GOPATH/bin/go-apidiff" "$GIT_BASE_REF" --compare-imports="false" --print-compatible="false" )
+set -e
+#popd || exit
+cmd_return_code=$?
+
+echo "Breaking changes finished with $cmd_return_code return code"
 
 if [ -z "$BREAKING_CHANGES" ]; then
-  echo "No breaking changes detected"
-else
-  if [ -z "$TARGET_BREAKING_CHANGES_FILE" ]; then
-    echo "Breaking changes detected"
-    echo "$BREAKING_CHANGES"
-  else
-    echo "Creating breaking changes file"
-    echo -e "# Breaking Changes\n## SDK changes\n$BREAKING_CHANGES\n## API Changelog\n https://www.mongodb.com/docs/atlas/reference/api-resources-spec/changelog" \
-		> "$script_path/../breaking_changes/${TARGET_BREAKING_CHANGES_FILE}.md"
-  fi
+    echo "No major breaking changes detected"
+    exit $cmd_return_code
 fi
+
+echo "Detected major breaking changes in the release"
+if [ -z "$TARGET_BREAKING_CHANGES_FILE" ]; then
+  echo "Breaking changes for the major release"
+  echo "$BREAKING_CHANGES"
+else
+  echo "Creating the breaking changes file with following breaking changes:"
+  echo "$BREAKING_CHANGES"
+  echo -e "# Breaking Changes\n## SDK changes\n$BREAKING_CHANGES\n## API Changelog\n https://www.mongodb.com/docs/atlas/reference/api-resources-spec/changelog" \
+  > "$script_path/../breaking_changes/${TARGET_BREAKING_CHANGES_FILE}.md"
+fi
+ 
+exit $cmd_return_code
