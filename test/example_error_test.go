89dd01bb3533dd2cbbb9c3adf6cf1e014b985d04
wtrocki <w.trocki@mongodb.com>
2024-10-04 13:56:18 +0100

    CLOUDP-276381: adding formatting to SDK

---
+++ test/example_error_test.go
@@ -0,0 +1,56 @@
+package test
+
+import (
+	"fmt"
+
+	"go.mongodb.org/atlas-sdk/v20240805004/admin"
+)
+
+ 
+func ExampleNewError() {
+	label := "test";
+	
+	err := admin.ApiError{
+		BadRequestDetail: &admin.BadRequestDetail{Fields: &[]admin.FieldViolation{
+			{Description: &label, Field: &label},
+		}},
+		Detail:           new(string),
+		Error:            new(int),
+		ErrorCode:        new(string),
+		Parameters:       &[]interface{}{},
+		Reason:           new(string),
+	}
+	fmt.Printf("Violations: %+v\n", admin.FormatErrorMessageWithDetails("200", "/test","POST", err))
+	// Output:
+	// Violations: POST /test: HTTP 200 (Error code: "") Detail:  Reason: . Params: [], BadRequestDetail: {"fields":[{"description":"test","field":"test"}]}
+}
+
+ 
+func ExampleNewErrorMissingFieldViolations() {
+	err := admin.ApiError{
+		BadRequestDetail: &admin.BadRequestDetail{},
+		Detail:           new(string),
+		Error:            new(int),
+		ErrorCode:        new(string),
+		Parameters:       &[]interface{}{},
+		Reason:           new(string),
+	}
+	fmt.Printf("%+v\n", admin.FormatErrorMessageWithDetails("200", "/test","POST", err))
+	// Output:
+	// POST /test: HTTP 200 (Error code: "") Detail:  Reason: . Params: [], BadRequestDetail: {}
+}
+
+
+func ExampleNewErrorMissingDetail() {
+	err := admin.ApiError{
+		BadRequestDetail: nil,
+		Detail:           new(string),
+		Error:            new(int),
+		ErrorCode:        new(string),
+		Parameters:       &[]interface{}{},
+		Reason:           new(string),
+	}
+	fmt.Printf("%+v\n", admin.FormatErrorMessageWithDetails("200", "/test","POST", err))
+	// Output:
+	// POST /test: HTTP 200 (Error code: "") Detail:  Reason: . Params: [], BadRequestDetail:
+}
