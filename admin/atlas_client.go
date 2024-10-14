af8f5e4c2defe7f94032ed901c67615f29513a37
wtrocki <w.trocki@mongodb.com>
2024-10-14 10:10:34 +0200

    fix: revert atlas_client integration

--- admin/atlas_client.go
+++ admin/atlas_client.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 
 	"github.com/mongodb-forks/digest"
-	"go.mongodb.org/atlas-sdk/v20240805004/auth/credentials"
 	"go.mongodb.org/atlas-sdk/v20240805004/internal/core"
 )
 
@@ -55,15 +54,6 @@ func UseDigestAuth(apiKey, apiSecret string) ClientModifier {
 		if err != nil {
 			return err
 		}
-		c.HTTPClient = httpClient
-		return nil
-	}
-}
-
-// TODO investigate how to integrate http client without overriding base path
-func UseServiceAccountAuth(client *credentials.OAuthClient) ClientModifier {
-	return func(c *Configuration) error {
-		httpClient := credentials.NewHTTPClientWithServiceAccountAuth(client);
 		c.HTTPClient = httpClient
 		return nil
 	}
