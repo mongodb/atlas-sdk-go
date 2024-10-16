ed9554343349be19a537e3fefe1b0fdcd21cc4c6
lantoli <430982+lantoli@users.noreply.github.com>
2024-10-16 13:28:38 +0000

    fix: required version bumps for Atlas SDK release

--- admin/atlas_client.go
+++ admin/atlas_client.go
@@ -1,4 +1,4 @@
-package admin // import "go.mongodb.org/atlas-sdk/v20240805004/admin"
+package admin // import "go.mongodb.org/atlas-sdk/v20240805005/admin"
 
 import (
 	"errors"
@@ -8,7 +8,7 @@ import (
 	"strings"
 
 	"github.com/mongodb-forks/digest"
-	"go.mongodb.org/atlas-sdk/v20240805004/internal/core"
+	"go.mongodb.org/atlas-sdk/v20240805005/internal/core"
 )
 
 const (
