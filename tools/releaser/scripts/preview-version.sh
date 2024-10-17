1835b2c2108616fbb424b30f51ea28cc425efbe0
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-10-17 07:38:10 +0200

    modify examples go.mod

--- tools/releaser/scripts/preview-version.sh
+++ tools/releaser/scripts/preview-version.sh
@@ -7,7 +7,15 @@ source "$script_path/extract-version.sh"
 # shellcheck source=/dev/null
 source "$script_path/version-paths.sh"
 
+examples_path="$script_path/../../../examples"
+
+echo "Delete specific version $SDK_RESOURCE_VERSION from examples go.mod"
+(cd "$examples_path" && go mod edit -droprequire="go.mongodb.org/atlas-sdk/$SDK_RESOURCE_VERSION")
+
 NEW_VERSION="preview"
 echo "Modifying all instances of version from $SDK_RESOURCE_VERSION to $NEW_VERSION across the repository."
 npm install
 npm exec -c "replace-in-file /$SDK_MAJOR_VERSION/g $NEW_VERSION $VERSION_UPDATE_PATHS_PREVIEW --isRegex"
+
+echo "Add preview version to examples go.mod"
+(cd "$examples_path" && go get go.mongodb.org/atlas-sdk/preview)
