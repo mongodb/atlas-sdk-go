96683b76eb4c370bf6c8c45c708ec0cdd4c12d13
lantoli <430982+lantoli@users.noreply.github.com>
2024-10-17 16:53:23 +0000

    temp: client update

--- examples/mock/cluster_test.go
+++ examples/mock/cluster_test.go
@@ -7,8 +7,8 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
-	"go.mongodb.org/atlas-sdk/v20240805005/admin"
-	"go.mongodb.org/atlas-sdk/v20240805005/mockadmin"
+	"github.com/mongodb/atlas-sdk-go/admin"
+	"github.com/mongodb/atlas-sdk-go/mockadmin"
 )
 
 
