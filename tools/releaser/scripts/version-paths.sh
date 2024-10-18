ce2323ea6c9a13404af0054c7296474d0952a3ef
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-10-18 16:34:16 +0200

    Revert "chore: Use a fix version for Preview" (#453)

--- tools/releaser/scripts/version-paths.sh
+++ tools/releaser/scripts/version-paths.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 export VERSION_UPDATE_PATHS="../**/go.mod,../**/*.go,../tools/**/*.mustache,../.mockery.yaml,../**/*.md"
-
-# don't change doc files to reduce Preview PR noise
-export VERSION_UPDATE_PATHS_PREVIEW="../**/go.mod,../**/*.go,../tools/**/*.mustache,../.mockery.yaml"
