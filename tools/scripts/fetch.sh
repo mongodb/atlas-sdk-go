137191af93302d3b6849539eeb7c13d3d2f472af
Bianca Lisle <bianca.vianadeaguiar@mongodb.com>
2025-02-20 16:43:16 +0000

    chore: update sdk to use mongodb/openapi

--- tools/scripts/fetch.sh
+++ tools/scripts/fetch.sh
@@ -17,32 +17,30 @@ set -o nounset
 ## OpenAPI file (latest)
 OPENAPI_FILE_NAME=${OPENAPI_FILE_NAME:-"atlas-api.yaml"}
 
-## Base URL
-API_BASE_URL=${API_BASE_URL:-"https://cloud.mongodb.com"}
-
 ## Folder used for fetching files
 OPENAPI_FOLDER=${OPENAPI_FOLDER:-"../openapi"}
 
-## S3 bucket where the spec is hosted
-S3_BUCKET=${S3_BUCKET:-"mongodb-mms-prod-build-server"}
+## Root V2 URL
+if [ "${BRANCH_NAME:-"main"}" == "dev-latest" ]; then
+    root_v2_url=https://raw.githubusercontent.com/mongodb/openapi/refs/heads/dev/openapi/v2
+else
+    root_v2_url=https://raw.githubusercontent.com/mongodb/openapi/refs/heads/main/openapi/v2
+fi
 
-versions_url="${API_BASE_URL}/api/openapi/versions"
 versions_file="versions.json"
 
 pushd "${OPENAPI_FOLDER}"
+versions_url="${root_v2_url}/${versions_file}"
 echo "Fetching versions from $versions_url"
 
 curl --show-error --fail --silent -o "${versions_file}" \
      -H "Accept: application/json" "${versions_url}"
 
 ## Dynamic Versioned API Version
-CURRENT_API_REVISION=$(jq -r '.versions."2.0" | .[-1]' < "./${versions_file}")
+CURRENT_API_REVISION=$(jq -r '.[-1]' < "./${versions_file}")
 
-echo "Fetching OpenAPI release sha"
-sha=$(curl --show-error --fail --silent -H "Accept: text/plain" "${API_BASE_URL}/api/private/unauth/version")
-
-echo "Fetching OAS file for ${sha}"
-openapi_url="https://${S3_BUCKET}.s3.amazonaws.com/openapi/${sha}-v2-${CURRENT_API_REVISION}.yaml"
+echo "Fetching OAS file for version $CURRENT_API_REVISION"
+openapi_url=${root_v2_url}/openapi-${CURRENT_API_REVISION}.yaml
 
 echo "Fetching api from $openapi_url to $OPENAPI_FILE_NAME"
 
