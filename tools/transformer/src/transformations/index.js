6c64297d00afb4ce770ff0d23e1d6d3d8e4b6835
Oriol Arbusi <oriol.abadal@mongodb.com>
2025-03-28 16:26:29 +0100

    fix index

--- tools/transformer/src/transformations/index.js
+++ tools/transformer/src/transformations/index.js
@@ -1,25 +1,36 @@
-const { applyAllOfTransformations } = require("./allOf");
-const { applyOneOfTransformations } = require("./oneOf");
-const { applyAnyOfTransformations } = require("./anyOf");
+const { applyAllOfTransformations, transformAllOf } = require("./allOf");
 const { applyModelNameTransformations } = require("./name");
+const {
+  applyOneOfTransformations,
+  transformOneOf,
+  transformOneOfProperties,
+} = require("./oneOf");
 const { applyDiscriminatorTransformations } = require("./discriminator");
+const { applyArrayTransformations } = require("./swapArray");
 const { applyRemoveEnumsTransformations } = require("./removeEnums");
-const { applyRemoveObjectAdditionalProperties } = require("./additionalPropertiesObject");
+const {
+  applyRemoveObjectAdditionalProperties,
+} = require("./additionalPropertiesObject");
+const { applyAnyOfTransformations } = require("./anyOf");
 const { applyRemoveNullableTransformations } = require("./removeNullable");
 const { removeRefsFromParameters } = require("./removeInvalidParams");
 const { reorderResponseBodies } = require("./reorderResponseBodies");
 const { applyFieldTransformations } = require("./fields");
 
 module.exports = {
+  applyModelNameTransformations,
+  transformAllOf,
+  transformOneOf,
+  transformOneOfProperties,
   applyAllOfTransformations,
   applyOneOfTransformations,
-  applyAnyOfTransformations,
-  applyModelNameTransformations,
   applyDiscriminatorTransformations,
+  applyArrayTransformations,
   applyRemoveEnumsTransformations,
   applyRemoveObjectAdditionalProperties,
+  applyAnyOfTransformations,
   applyRemoveNullableTransformations,
   removeRefsFromParameters,
   reorderResponseBodies,
-  applyFieldTransformations,
+  applyFieldTransformations
 };
