d6cc8545f0491ff4dd6d968aa885069234b28a46
Aastha Mahendru <aastha.mahendru@mongodb.com>
2025-04-01 23:25:30 +0100

    mockery

--- tools/scripts/generate_mocks.sh
+++ tools/scripts/generate_mocks.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 rm -Rf ../mockadmin
 
-# install only if not already present
-if ! which mockery; then
-	docker run --rm -u "$(id -u):$(id -g)" -v "$(dirname "$PWD")":/src --workdir=/src vektra/mockery:v2.42.1
-else
+# Use the locally installed mockery
+if which mockery >/dev/null 2>&1; then
 	mockery --dir ../mockadmin
+else
+	echo "mockery not found; please ensure it is installed."
+	exit 1
 fi
 
-# Use any instead of interface{} in genereated mocks
+# Use any instead of interface{} in generated mocks
 npm install
 npm exec -c "replace-in-file /interface{}/g any  ../mockadmin/**/*.go --isRegex"
