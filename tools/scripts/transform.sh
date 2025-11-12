cf311553b61135f13f26462ac86d769cc3e5ac73
Agustin Bettati <bettatiagustin@gmail.com>
2025-11-12 14:15:51 +0100

    adjust usages of transform to leverage cli directly

--- tools/scripts/transform.sh
+++ tools/scripts/transform.sh
@@ -19,4 +19,4 @@ echo "# Running transformation from $transformed_file OpenAPI from $OPENAPI_FILE
 cp "$OPENAPI_FOLDER/$OPENAPI_FILE_NAME" "$openapiFileLocation"
 
 npm install
-npm run sdk:transform -- "$openapiFileLocation"
+node ./transformer/src/cli.js transform --input "$openapiFileLocation" --output "$openapiFileLocation"
